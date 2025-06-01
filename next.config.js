7948d453869477d31396a6075030992d4c89529c
Nurmukhammed <nur.omirzaq@gmail.com>
2025-06-01 13:47:51 +0500

    feat: remove unneccessary dependencies

--- next.config.js
+++ next.config.js
@@ -16,40 +16,6 @@ const nextConfig = {
       },
     ]
   },
-  webpack: (config, { isServer }) => {
-    // Ignore MongoDB optional dependencies that cause issues in browser environment
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        stream: false,
-        url: false,
-        zlib: false,
-        http: false,
-        https: false,
-        assert: false,
-        os: false,
-        path: false,
-      };
-    }
-
-    // Ignore problematic MongoDB optional dependencies
-    config.externals = config.externals || [];
-    config.externals.push({
-      'snappy': 'commonjs snappy',
-      'aws4': 'commonjs aws4',
-      'mongodb-client-encryption': 'commonjs mongodb-client-encryption',
-      'kerberos': 'commonjs kerberos',
-      'bson-ext': 'commonjs bson-ext',
-      'gcp-metadata': 'commonjs gcp-metadata',
-      'saslprep': 'commonjs saslprep',
-    });
-
-    return config;
-  },
 }
 
 module.exports = nextConfig 
